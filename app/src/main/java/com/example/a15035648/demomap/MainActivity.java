4b94511fa22dc7c43bca88b12bf5258655637bfd
15035648 <15035648@myrp.edu.sg>
2017-07-13 12:11:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15035648/demomap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a15035648.demomap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
